f599b6f8b69bbf6f049d0a6b4f4fc706fef859ca
Son1kXDev <son1kx@vk.com>
2024-09-18 18:46:14 +0300

    add toastr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import {Component, ElementRef, inject, OnInit, ViewChild} from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NgIf} from "@angular/common";
-import {AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import { NgIf } from '@angular/common';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NgIf, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   isLoading = false;
   shortLink = '';
 
@@ -18,12 +24,16 @@ export class AppComponent implements OnInit{
   formBuilder = inject(FormBuilder);
 
   shorter = this.formBuilder.group({
-    inputURL:['',[Validators.required, Validators.pattern(this.reg)]]
-  })
+    inputURL: ['', [Validators.required, Validators.pattern(this.reg)]],
+  });
 
-  onSubmitForm(){
+  toastrService = inject(ToastrService);
+
+  showError() {
+    this.toastrService.error('Invalid URL');
   }
 
-  ngOnInit(): void {
-  }
+  onSubmitForm() {}
+
+  ngOnInit(): void {}
 }

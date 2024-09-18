b19809699de78724fa90e2b06726f423ee52cfaf
Son1kXDev <son1kx@vk.com>
2024-09-18 16:29:59 +0300

    fisrt commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, inject, OnInit, ViewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgIf} from "@angular/common";
+import {AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'shorter';
+export class AppComponent implements OnInit{
+  isLoading = false;
+  shortLink = '';
+
+  onSubmitForm(){
+  }
+
+  ngOnInit(): void {
+  }
 }
